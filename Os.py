b4c0338243ff88c42b669713e441ac56b9e53cd9
DX-CYBER BOY <116189487+DX-CYBER505@users.noreply.github.com>
2023-01-12 00:12:49 +0600

    Update Os.py

--- Os.py
+++ Os.py
@@ -1,10 +1,22 @@
-import os,platform
-os.system('clear')
-print('[•] Checking Updates...')
-os.system('git pull')
-bit = platform.architecture()[0]
-if bit=='64bit':
-    print('\033[1;32m[•] Congrats! Your Device Support This Tools>
-    os.system('xdg-open https://youtube.com/@dxcyberboy1056>
-    import Love_enc
-    Love_enc.menu()
+import os, sys
+
+try:
+
+    __import__("Love_enc").main()
+
+except Exception as e:
+
+    exit(str(e))
+
+ 
+   
+
+              
+              
+           
+              
+              
+              
+              
+
+
